35ec55d11a63c40894d96bd77b6ccd6841fb2db4
Sunki Baek <sunki.baek@gmail.com>
2020-08-19 11:21:59 -0700

    feat: add billing for installing app

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require("koa-session");
 dotenv.config();
 const { default: graphQLProxy } = require("@shopify/koa-shopify-graphql-proxy");
 const { ApiVersion } = require("@shopify/koa-shopify-graphql-proxy");
+const getSubscriptionUrl = require("./server/getSubscriptionUrl");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -29,7 +30,7 @@ app.prepare().then(() => {
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: ["read_products", "write_products"],
-      afterAuth(ctx) {
+      async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set("shopOrigin", shop, {
           httpOnly: false,
@@ -37,6 +38,7 @@ app.prepare().then(() => {
           sameSite: "None",
         });
         ctx.redirect("/");
+        await getSubscriptionUrl(ctx, accessToken, shop);
       },
     })
   );
