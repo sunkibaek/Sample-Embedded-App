5448651041568ee49471fd77f4de7fadd37b7710
Sunki Baek <sunki.baek@gmail.com>
2020-08-19 09:56:15 -0700

    feat: add to use store-js to pass selected product ids

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { EmptyState, Layout, Page } from "@shopify/polaris";
 import { ResourcePicker, TitleBar } from "@shopify/app-bridge-react";
+import store from "store-js";
+import ResourceListWithProducts from "../components/ResourceList";
 
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
@@ -34,6 +36,8 @@ class Index extends React.Component {
           >
             <p>Select products to change their price temporarily.</p>
           </EmptyState>
+
+          <ResourceListWithProducts />
         </Layout>
       </Page>
     );
@@ -44,7 +48,7 @@ class Index extends React.Component {
 
     this.setState({ open: false });
 
-    console.log(idsFromResources);
+    store.set("ids", idsFromResources);
   };
 }
 
