fff21fa967c11bbc58425ecbf98932579d1ee0dc
Sunki Baek <sunki.baek@gmail.com>
2020-08-19 10:50:14 -0700

    feat: edit to show empty state properly

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 class Index extends React.Component {
   state = { open: false };
   render() {
+    const emptyState = !store.get("ids");
+
     return (
       <Page>
         <TitleBar
@@ -25,20 +27,22 @@ class Index extends React.Component {
           onCancel={() => this.setState({ open: false })}
         />
 
-        <Layout>
-          <EmptyState
-            heading="Discount your products temporarily"
-            action={{
-              content: "Select products",
-              onAction: () => this.setState({ open: true }),
-            }}
-            image={img}
-          >
-            <p>Select products to change their price temporarily.</p>
-          </EmptyState>
-
+        {emptyState ? (
+          <Layout>
+            <EmptyState
+              heading="Discount your products temporarily"
+              action={{
+                content: "Select products",
+                onAction: () => this.setState({ open: true }),
+              }}
+              image={img}
+            >
+              <p>Select products to change their price temporarily.</p>
+            </EmptyState>
+          </Layout>
+        ) : (
           <ResourceListWithProducts />
-        </Layout>
+        )}
       </Page>
     );
   }
